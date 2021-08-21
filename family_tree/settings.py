80575d64a6e82251444c4a5a1efe44e2c3d0f9d6
Kolenich <nick.zhigalin@gmail.com>
2021-08-21 23:53:11 +0300

    Автоудаление ненужных файлов из системы.

--- family_tree/settings.py
+++ family_tree/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'members.apps.MembersConfig',
+    'members',
+    'django_cleanup'
 ]
 
 MIDDLEWARE = [
